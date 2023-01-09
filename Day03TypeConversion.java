b539f8f26170e2cd807a7fd3cac44dc9b063abc3
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-09 09:01:09 -0600

    casts | answers

--- Day03TypeConversion.java
+++ Day03TypeConversion.java
@@ -4,16 +4,16 @@ public class Day03TypeConversion {
 
         // converts to int by truncating decimals
         // predict the output
-        System.out.println((int) 2.71828);
-        System.out.println((int) -2.71828);
+        System.out.println((int) 2.71828); // 2
+        System.out.println((int) -2.71828); // -2
         System.out.println();
 
-        System.out.println((int) Math.round(2.71828));
-        System.out.println((int) Math.round(-2.71828));
+        System.out.println((int) Math.round(2.71828)); // 3
+        System.out.println((int) Math.round(-2.71828)); // -3
         System.out.println();
 
-        System.out.println(11 * (int) 0.25);
-        System.out.println((int) 11 * 0.25);
-        System.out.println((int) (11 * 0.25));
+        System.out.println(11 * (int) 0.25); // 0 (int)
+        System.out.println((int) 11 * 0.25); // 2.75 (double)
+        System.out.println((int) (11 * 0.25)); // 2 (int)
     }
 }
