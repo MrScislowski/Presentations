7e1aaf41e6a83ae1315c1d491f66fae1f2a8d98e
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-09 08:32:00 -0600

    automatic conversions | answers

--- Day03TypeConversion.java
+++ Day03TypeConversion.java
@@ -3,24 +3,24 @@ public class Day03TypeConversion {
         // AUTOMATIC (IMPLICIT) CONVERSIONS
 
         // int with int operations. Predict value and type of result...
-        System.out.println(2 + 3);
-        System.out.println(2 - 3);
-        System.out.println(2 * 3);
-        System.out.println(2 / 3);
+        System.out.println(2 + 3); // 5 (int)
+        System.out.println(2 - 3); // -1 (int)
+        System.out.println(2 * 3); // 6 (int)
+        System.out.println(2 / 3); // 0 (int)
 
         // int with double operations
-        System.out.println(2 + 3.0);
-        System.out.println(2 - 3.0);
-        System.out.println(2 * 3.0);
-        System.out.println(2 / 3.0);
+        System.out.println(2 + 3.0); // 5.0 (double)
+        System.out.println(2 - 3.0); // -1.0 (double)
+        System.out.println(2 * 3.0); // 6.0 (double)
+        System.out.println(2 / 3.0); // 0.666666... (double)
 
         // String with number
-        System.out.println("2" + 2);
-        System.out.println(2.0 + "2");
+        System.out.println("2" + 2); // "22" (String)
+        System.out.println(2.0 + "2"); // "2.02" (String)
 
         // multiple operations
-        System.out.println((7/2) * 2.0);
-        System.out.println((7/2.0) * 2);
+        System.out.println((7/2) * 2.0); // 6.0 (double)
+        System.out.println((7/2.0) * 2); // 7.0 (double)
 
     }
 }
