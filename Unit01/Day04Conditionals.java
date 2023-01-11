ae84491e04fc069ec9436795cd0713f8118e9d79
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-11 12:25:40 -0600

    conditionals | TwoSort

--- Unit01/Day04Conditionals.java
+++ Unit01/Day04Conditionals.java
@@ -1,13 +1,14 @@
 public class Day04Conditionals {
     public static void main(String[] args) {
-        // CoinFlip
-        double rand = Math.random();
-        if (rand < 0.5) {
-            System.out.println("heads");
-        } else {
-            System.out.println("tails");
-        }
         // TwoSort
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        if (a < b) {
+            System.out.println(a + ", " + b);
+        } else {
+            System.out.println(b + ", " + a);
+        }
 
         // RandInt
 
