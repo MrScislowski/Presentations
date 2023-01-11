5a3dec17fe9a39153d4e9d514f5c7c1d338dd7aa
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-11 12:29:44 -0600

    IntOps w/ error checking

--- Unit01/Day04Conditionals.java
+++ Unit01/Day04Conditionals.java
@@ -1,15 +1,25 @@
 public class Day04Conditionals {
     public static void main(String[] args) {
-        // RandInt w/ error checking
-        if (args.length < 1) {
-            System.out.println("Please provide one command line argument");
+        // IntOps w/ error checking
+        if (args.length < 2) {
+            System.out.println("Please provide two command line arguments");
         } else {
-            int upperLimit = Integer.parseInt(args[0]);
-            int randInt = (int) (Math.random() * upperLimit);
-            System.out.println(randInt);
+            int first = Integer.parseInt(args[0]);
+            int second = Integer.parseInt(args[1]);
+    
+            System.out.println(first + " + " + second + " = " + (first + second));
+            System.out.println(first + " - " + second + " = " + (first - second));
+            System.out.println(first + " * " + second + " = " + (first * second));
+            if (second == 0) {
+                System.out.println("Cannot divide by zero");
+            } else {
+                System.out.println(first + " / " + second + " = " + (first / second));
+                System.out.println(first + " % " + second + " = " + (first % second));
+            }
+            
         }
+        
 
 
-        // IntOps
     }
 }
