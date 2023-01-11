92b11cb7e72e33159cc613d7a7225f712e4392e2
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-11 12:24:45 -0600

    conditionals | CoinFlip

--- Unit01/Day04Conditionals.java
+++ Unit01/Day04Conditionals.java
@@ -1,7 +1,12 @@
 public class Day04Conditionals {
     public static void main(String[] args) {
         // CoinFlip
-
+        double rand = Math.random();
+        if (rand < 0.5) {
+            System.out.println("heads");
+        } else {
+            System.out.println("tails");
+        }
         // TwoSort
 
         // RandInt
