47691c9c3d2abcbe54acab97d8a9edf74efc4a17
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-13 11:27:51 -0600

    conditionals | ThreeEqual w/o boolean operators

--- Unit01/Day04Conditionals.java
+++ Unit01/Day04Conditionals.java
@@ -1,23 +1,20 @@
 public class Day04Conditionals {
     public static void main(String[] args) {
-        // IntOps w/ error checking
-        if (args.length < 2) {
-            System.out.println("Please provide two command line arguments");
-        } else {
-            int first = Integer.parseInt(args[0]);
-            int second = Integer.parseInt(args[1]);
-    
-            System.out.println(first + " + " + second + " = " + (first + second));
-            System.out.println(first + " - " + second + " = " + (first - second));
-            System.out.println(first + " * " + second + " = " + (first * second));
-            if (second == 0) {
-                System.out.println("Cannot divide by zero");
+        // ThreeEqual
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+        if (a == b) {
+            if (c == a) {
+                System.out.println("equal");
             } else {
-                System.out.println(first + " / " + second + " = " + (first / second));
-                System.out.println(first + " % " + second + " = " + (first % second));
+                System.out.println("not equal");
             }
-            
+        } else {
+            System.out.println("not equal");
         }
+
         
 
 
