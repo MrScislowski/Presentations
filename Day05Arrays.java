2b9dcda825f600e0502652d6b56c4038f4253049
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-17 11:52:29 -0600

    arrays | creating (application) (reverse3)

--- Day05Arrays.java
+++ Day05Arrays.java
@@ -2,20 +2,18 @@ import java.util.Arrays;
 
 public class Day05Arrays {
     public static void main(String[] args) {
-        // declaring / creating / initializing arrays
+        // reverse3
 
-        // long form way
-        int[] nums; // declare
-        nums = new int[3]; // initialize / create the array
-        nums[0] = 3;
-        nums[1] = 1;
-        nums[2] = 4;
+        // Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
+        int[] nums = {5, 11, 9};
 
-        // shorthand
-        int[] nums2 = {3, 1, 4};
+        int[] newNums;
+        newNums = new int[] {nums[2], nums[1], nums[0]};
+        System.out.println(Arrays.toString(newNums));
 
-        // intermediate
-        nums = new int[] {2, 7, 1};
+
+
+        
 
 
 
