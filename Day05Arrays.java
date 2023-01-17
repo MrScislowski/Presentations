395a25eb0c310aa80089a423bd0e14c0a72016d4
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-17 11:41:24 -0600

    arrays | modifying (fix23)

--- Day05Arrays.java
+++ Day05Arrays.java
@@ -1,13 +1,16 @@
+import java.util.Arrays;
+
 public class Day05Arrays {
     public static void main(String[] args) {
-        // firstLast6
-        // Given an array of ints, return true if 6 appears as either the first or last element in the array. The array will be length 1 or more.
-        
-        int[] nums = {4, 6, 1, 2, 6};
-        if (nums[0] == 6 || nums[nums.length - 1] == 6) {
-            System.out.println(true);
-        } else {
-            System.out.println(false);
+        // fix23
+        // Given an int array length 3, if there is a 2 in the array immediately followed by a 3, set the 3 element to 0. Return the changed array.
+
+        int[] nums = {1, 2, 3};
+        if (nums[0] == 2 && nums[1] == 3) {
+            nums[1] = 0;
+        } else if (nums[1] == 2 && nums[2] == 3) {
+            nums[2] = 0;
         }
+        System.out.println(Arrays.toString(nums));
     }
 }
