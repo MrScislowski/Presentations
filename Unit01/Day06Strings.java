41e499503ad0dcebcabdf4140db8dc66ab8178cb
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-24 11:29:42 -0600

    Strings | indexOf using command line arguments

--- Unit01/Day06Strings.java
+++ Unit01/Day06Strings.java
@@ -1,11 +1,10 @@
 public class Day06Strings {
-    public static void main(String[] args
-    ) {
+    public static void main(String[] args) {
         // indexOf demo
 
-        String text = "the quick fox jumped over";
-        String searchTerm = "fob";
-        System.out.println("indexOf returned: " + text.indexOf(searchTerm));
+        String text = args[0];
+        String searchTerm = args[1];
+        System.out.println(searchTerm + " occurs at position " + text.indexOf(searchTerm));
         
     }
 }
