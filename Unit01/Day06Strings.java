78c9d10ec44c31d3477dbd9a28b1573c3c958cd5
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-24 11:35:10 -0600

    Strings | indexOf using Scanner

--- Unit01/Day06Strings.java
+++ Unit01/Day06Strings.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Day06Strings {
     public static void main(String[] args) {
         // indexOf demo
 
-        String text = args[0];
-        String searchTerm = args[1];
+        System.out.println("please type in the long text you want to search within");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        
+        System.out.println("Thanks, now enter the search term");
+        String searchTerm = scanner.nextLine();
+
         System.out.println(searchTerm + " occurs at position " + text.indexOf(searchTerm));
+
+        scanner.close();
         
     }
 }
