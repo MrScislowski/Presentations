183657e30d1f226568fe19c9e5b78a76fdbdb64b
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-24 11:48:32 -0600

    Strings | indexOf for webpage parsing

--- Unit01/Day06Strings.java
+++ Unit01/Day06Strings.java
@@ -2,16 +2,22 @@ import java.util.Scanner;
 
 public class Day06Strings {
     public static void main(String[] args) {
-        // indexOf demo
+        // indexOf to parse HTML
 
-        System.out.println("please type in the long text you want to search within");
+        System.out.println("please type in the HTML you want to search within");
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
         
         System.out.println("Thanks, now enter the search term");
         String searchTerm = scanner.nextLine();
 
-        System.out.println(searchTerm + " occurs at position " + text.indexOf(searchTerm));
+        int textPos = text.indexOf(searchTerm);
+        int closingTagStartPos = text.indexOf("</", textPos);
+        int closingTagEndPos = text.indexOf(">", closingTagStartPos);
+
+        System.out.print("The text you entered was within a ");
+        System.out.print(text.substring(closingTagStartPos+2, closingTagEndPos));
+        System.out.println(" tag");
 
         scanner.close();
         
