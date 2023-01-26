a5241543e9897648e67da274a3cbfd6451a3f534
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-26 11:38:14 -0600

    Strings | indexOf to parse sentences

--- Unit01/Day06Strings.java
+++ Unit01/Day06Strings.java
@@ -2,24 +2,25 @@ import java.util.Scanner;
 
 public class Day06Strings {
     public static void main(String[] args) {
-        // indexOf to parse HTML
+        // indexOf to parse sentences
 
-        System.out.println("please type in the HTML you want to search within");
+        System.out.println("please type in the sentences");
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
         
-        System.out.println("Thanks, now enter the search term");
+        System.out.println("Thanks, now enter the search word");
         String searchTerm = scanner.nextLine();
 
-        int textPos = text.indexOf(searchTerm);
-        int closingTagStartPos = text.indexOf("</", textPos);
-        int closingTagEndPos = text.indexOf(">", closingTagStartPos);
-
-        System.out.print("The text you entered was within a ");
-        System.out.print(text.substring(closingTagStartPos+2, closingTagEndPos));
-        System.out.println(" tag");
+        int wordPos = text.indexOf(searchTerm);
+        int endOfSentence = text.indexOf(".", wordPos);
+        String textUpToWord = text.substring(0, wordPos);
+        int startOfSentence = textUpToWord.lastIndexOf(".");
+        System.out.println("the word " + searchTerm + " occurs in sentence");
+        System.out.println(text.subSequence(startOfSentence+2, endOfSentence+1));
 
         scanner.close();
         
     }
 }
+
+// javac Day06Strings.java; cat sentences.txt | java Day06Strings
