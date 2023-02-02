ceaf87a41c032d8557aa79af938c186dbdb47df6
Daniel Scislowski <mr.scislowski@gmail.com>
2023-02-02 11:32:09 -0600

    implement printPowersOfTwo

--- Unit02/Topic02WhileLoops.java
+++ Unit02/Topic02WhileLoops.java
@@ -12,9 +12,27 @@ public class Topic02WhileLoops {
         }
     }
 
+    public static void printPowersOfTwo(int maxPower) {
+        int curPower = 0;
+        int result = 1;
+
+        while (curPower <= maxPower) {
+            System.out.println("2^" + curPower + " = " + result);
+            result = result * 2;
+            curPower = curPower + 1;
+        }
+    }
+
     public static void main(String[] args) {
-        printTenHellos();
+        int maxPowerOfTwo = Integer.parseInt(args[0]);
+
+        // printTenHellos();
+
+        // last line of printPowersOfTwo(31) is:
+        // 2^31 = -2147483648
+        // due to overflow
+        printPowersOfTwo(maxPowerOfTwo);
     }
 }
 
-// javac .\Topic02WhileLoops.java; java Topic02WhileLoops
+// javac .\Topic02WhileLoops.java; java Topic02WhileLoops 28
