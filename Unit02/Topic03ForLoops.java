6b184d1e341da8a08119f373c6668e0c78b53402
Daniel Scislowski <mr.scislowski@gmail.com>
2023-02-03 11:45:07 -0600

    implement codingBat exercises sum67 and zeroMax

--- Unit02/Topic03ForLoops.java
+++ Unit02/Topic03ForLoops.java
@@ -23,6 +23,45 @@ public class Topic03ForLoops {
         }
     }
 
+    public static int sum67(int[] nums) {
+        int sum = 0;
+        boolean countingPaused = false;
+
+        for (int index = 0; index < nums.length; index = index + 1) {
+            if (nums[index] == 6) {
+                countingPaused = true;
+            }
+            if (!countingPaused) {
+                sum += nums[index];
+            }
+            if (nums[index] == 7) {
+                countingPaused = false;
+            }
+        }
+        return sum;
+    }
+
+    public static int[] zeroMax(int[] nums) {
+        // make a copy of the array
+        int[] a = new int[nums.length];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = nums[i];
+        }
+        
+        int largestOddValueSoFar = 0;
+        for (int index=a.length-1; index >= 0; index = index - 1) {
+            if (a[index] == 0) {
+                a[index] = largestOddValueSoFar;
+            }
+            if (a[index] % 2 == 1 && a[index] > largestOddValueSoFar) {
+                largestOddValueSoFar = a[index];
+            }
+        }
+        return a;
+    }
+    
+    
+
     public static void main(String[] args) {
         printTenHellos();
     }
