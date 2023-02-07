9d9c1f2d24e76b7856a694b618c31a9a29b1c36d
Daniel Scislowski <mr.scislowski@gmail.com>
2023-02-07 11:30:37 -0600

    add dice simulation code

--- Unit02/Topic03ForLoops.java
+++ Unit02/Topic03ForLoops.java
@@ -13,6 +13,36 @@
  */
 public class Topic03ForLoops {
 
+    private static int rollDie() {
+        int result = (int) (Math.random() * 6) + 1;
+        return result;
+    }
+
+    public static void simulationExample() {
+        // rolling two dice... what's the probability that you roll a total of 5?
+        // what about 6?
+
+        final int NTRIALS = 100000;
+
+        int fiveCount = 0;
+        int sixCount = 0;
+
+        for (int trial = 0; trial < NTRIALS; trial++) {
+            int roll1 = rollDie();
+            int roll2 = rollDie();
+            int rollSum = roll1 + roll2;
+
+            if (rollSum == 5) {
+                fiveCount++;
+            } else if (rollSum == 6) {
+                sixCount++;
+            }
+        }
+        System.out.println("probability of rolling a 5: " + (double) fiveCount / NTRIALS);
+        System.out.println("probability of rolling a 6: " + (double) sixCount / NTRIALS);
+
+    }
+
     public static void printTenHellos() {
         System.out.println("1st Hello");
         System.out.println("2nd Hello");
@@ -63,7 +93,7 @@ public class Topic03ForLoops {
     
 
     public static void main(String[] args) {
-        printTenHellos();
+        simulationExample();
     }
 }
 // javac .\Topic03ForLoops.java; java Topic03ForLoops
