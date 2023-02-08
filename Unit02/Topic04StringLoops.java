56710388d9a5e11f14a1b4e8a3eca1fc5fcffded
Daniel Scislowski <mr.scislowski@gmail.com>
2023-02-08 11:16:29 -0600

    countCode solution

--- Unit02/Topic04StringLoops.java
+++ Unit02/Topic04StringLoops.java
@@ -11,6 +11,17 @@ public class Topic04StringLoops {
         return count;
     }
 
+    public static int countCode(String str) {
+        int count = 0;
+        for (int pos = 0; pos < str.length()-3; pos++) {
+            if (str.substring(pos, pos+2).equals("co") &&
+                str.substring(pos+3, pos+4).equals("e")) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
 
     public static void main(String[] args) {
         System.out.println(countHi("heighiten"));
