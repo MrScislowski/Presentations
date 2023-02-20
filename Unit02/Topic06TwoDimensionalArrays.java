c14327769e46d812532dc53dd46abdb6afe3d162
Daniel Scislowski <mr.scislowski@gmail.com>
2023-02-20 11:36:36 -0600

    declare & initialize

---
+++ Unit02/Topic06TwoDimensionalArrays.java
@@ -0,0 +1,40 @@
+public class Topic06TwoDimensionalArrays {
+
+    public static void main(String[] args) {
+        // declare and initialize 2D array
+        int[][] a = new int[][] {
+            {1, 2, 3},
+            {4, 5, 6}, 
+            {7, 8, 9}};
+
+        System.out.println(a[1][2]);
+
+
+        // traverse 2D square array to have all values be 1
+
+        // traverse 2D rectangular array to have all values be 1
+
+        // traverse 2D ragged array to have all values be 1
+
+        // write our own code to pring 2d array
+
+        // withAverages
+
+
+
+        // double[][] a = new double[][] {
+        //     {99, 85, 98},
+        //     {98, 57, 79},
+        //     {92, 77, 74},
+        //     {94, 62, 81},
+        //     {99, 94, 92},
+        //     {80, 76.5, 67},
+        //     {76, 58.5, 90.5},
+        //     {92, 66, 91},
+        //     {77, 70.5, 66.5},
+        //     {89, 89.5, 81}
+        // };
+    }
+}
+
+// javac Topic06TwoDimensionalArrays.java; java Topic06TwoDimensionalArrays
