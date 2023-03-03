844b1638d8d09cf25f70f4c6f2f5b6ba247c07ed
Daniel Scislowski <mr.scislowski@gmail.com>
2023-03-03 11:25:27 -0600

    this keyword to aid variable naming

---
+++ Unit03/EarthLocation.java
@@ -0,0 +1,13 @@
+public class EarthLocation {
+
+    // INSTANCE VARIABLES
+    private double latitude;
+    private double longitude;
+    
+    // CONSTRUCTOR
+    public EarthLocation(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+    
+}
