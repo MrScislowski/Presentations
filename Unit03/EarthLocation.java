acdceb9b7399379bca5f24c544f526e931aebbe3
Daniel Scislowski <mr.scislowski@gmail.com>
2023-03-03 11:46:33 -0600

    implement distanceTo instance method

--- Unit03/EarthLocation.java
+++ Unit03/EarthLocation.java
@@ -16,7 +16,26 @@ public class EarthLocation {
         this.longitude = (Math.random() * 2 - 1)*180;
     }
 
-    // INSTANCE METHOD
+    // INSTANCE METHODS
+    public double distanceTo(EarthLocation other) {
+        // radius of earth in miles
+        final double rE = 3963.0;
+
+        double lat1 = Math.toRadians(this.latitude);
+        double lat2 = Math.toRadians(other.latitude);
+        double lon1 = Math.toRadians(this.longitude);
+        double lon2 = Math.toRadians(other.longitude);
+
+        // great circle formula
+        double resultingDistance = rE * 
+            Math.acos(
+                Math.sin(lat1)*Math.sin(lat2) +
+                Math.cos(lat1)*Math.cos(lat2)*Math.cos(lon1-lon2)
+            );
+        
+        return resultingDistance;
+    }
+
     public String toString() {
         // make it look like this:
         // 37.6872° N, 97.3301° W
@@ -48,6 +67,14 @@ public class EarthLocation {
             EarthLocation randLoc = new EarthLocation();
             System.out.println(randLoc);
         }
+
+        // EarthLocation kc = 39.0997° N, 94.5786° W;
+        EarthLocation kc = new EarthLocation(39.0997, -94.5786);
+        EarthLocation ict = new EarthLocation(37.6872, -97.3301);
+
+        System.out.println("Distance from wichita to kc is: ");
+        System.out.println(ict.distanceTo(kc));
+
     }
 
     
