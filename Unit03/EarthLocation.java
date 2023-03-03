860813a2a1edf6dfdcb2b8d292af9e9618d547ff
Daniel Scislowski <mr.scislowski@gmail.com>
2023-03-03 11:32:15 -0600

    implement toString

--- Unit03/EarthLocation.java
+++ Unit03/EarthLocation.java
@@ -9,5 +9,37 @@ public class EarthLocation {
         this.latitude = latitude;
         this.longitude = longitude;
     }
+
+    // INSTANCE METHOD
+    public String toString() {
+        // make it look like this:
+        // 37.6872° N, 97.3301° W
+        String result = "";
+
+        if (latitude < 0) {
+            result += (latitude * -1);
+            result += "° S";
+        } else {
+            result += latitude;
+            result += "° N";
+        }
+
+        result += ", ";
+
+        if (longitude < 0) {
+            result += (longitude * -1);
+            result += "° W";
+        } else {
+            result += longitude;
+            result += "° E";
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        EarthLocation wichita = new EarthLocation(37.6872, -97.3301);
+        System.out.println(wichita);
+    }
+
     
 }
