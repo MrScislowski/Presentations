e8757121b357f3ad23ff1bc868077d0c881fd100
Daniel Scislowski <mr.scislowski@gmail.com>
2023-03-09 12:05:44 -0600

    added JUnit tests for distanceTo

---
+++ Unit03/EarthLocationTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
+
+public class EarthLocationTest {
+
+    @Test
+    public void testDistanceTo() {
+        double expectedDistance, calculatedDistance;
+        double tolerance;
+
+        // two locations on top of one another
+        EarthLocation l1 = new EarthLocation(37, -45);
+        EarthLocation l2 = new EarthLocation(37, -45);
+        expectedDistance = 0.0;
+        calculatedDistance = l1.distanceTo(l2);
+        assertEquals(expectedDistance, calculatedDistance);
+
+        // http://amsi.org.au/ESA_Senior_Years/SeniorTopic8/8b/8b_2content_9.html
+        EarthLocation sydney = new EarthLocation(-33.5, 151.13);
+        EarthLocation capeTown = new EarthLocation(-33.5, 18.22);
+        expectedDistance = 6921.3486986;
+        calculatedDistance = sydney.distanceTo(capeTown);
+        tolerance = 100;
+        assertEquals(expectedDistance, calculatedDistance, tolerance);
+
+
+    }
+}
